b2a394435985514a13186a75daa4ea9c8e0e94c5
kaclr <kaclr@foxmail.com>
2015-11-14 15:12:21 +0800

    在开始、读档、选关界面按下按钮后增加加载中的窗口提示

---
+++ Assets/ScriptFolder/ButtonLoadShow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonLoadShow : MonoBehaviour
+{
+
+    public GameObject LoadUIPrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnClick()
+    {
+        //单击时生成载入提示
+        GameObject temp =  Instantiate(LoadUIPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+    }
+}
