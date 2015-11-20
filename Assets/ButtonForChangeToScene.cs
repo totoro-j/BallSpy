5330665976812bd5fae80883e29ba5dc99eda2ab
totoro-j <250448196@qq.com>
2015-11-20 16:04:19 +0800

    UI文件夹的补充
    Signed-off-by: totoro-j <250448196@qq.com>

---
+++ Assets/ButtonForChangeToScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonForChangeToScene : MonoBehaviour
+{
+    public string SceneName;
+    // Use this for initialization
+
+    //通过外部调用本函数来进行场景变更
+    public void ChangeScene()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel(SceneName);//回到主菜单
+    }
+}
