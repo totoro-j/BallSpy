f45015523517089421cad8eb4b3749f8366449c7
kaclr <kaclr@foxmail.com>
2015-11-20 18:47:50 +0800

    改变了结算画面的数量设定，支持1-7个不同数量星星的显示

--- Assets/ScriptFolder/SettlementScene/StarControl.cs
+++ Assets/ScriptFolder/SettlementScene/StarControl.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class StarControl : MonoBehaviour
 {
     //星星数量
     public int StarNumber;
 
+    //星星总数
+    public int StarSum;
+
+    //动画间隔
+    public float AnimInterval;
+
     //开启星级评价
     public bool StarEnable;
 
-    public GameObject[] StarObjects;
+    //物体下挂载的一个星星样本
+    private GameObject _starSample;
+//
+//    [HideInInspector]
+//    public List<GameObject> StarObjectsList;
     // Use this for initialization
     void Awake()
     {
@@ -19,10 +31,44 @@ public class StarControl : MonoBehaviour
         }
         else
         {
-            foreach (GameObject  star in StarObjects)
+            //加入当前第一个挂载的星星
+            _starSample = GetAChild(gameObject);
+
+            GetAChild(_starSample).GetComponent<WinStar>().StarNumber = StarNumber;
+//            print(_starSample);
+//            StarObjectsList.Add(GetAChild(_starSample));
+
+
+            if (StarSum > 1)
             {
-                star.GetComponent<WinStar>().StarNumber = StarNumber;
+                for (int i = 1; i < StarSum; i++)
+                {
+                    //生成整体对象
+                   GameObject starback =  Instantiate(_starSample, transform.position, Quaternion.identity) as GameObject;
+                  
+                    //挂在本网格物体下
+                    starback.transform.parent = gameObject.transform;
+                    starback.transform.localScale = Vector3.one;
+                    //获取子对象
+                    GameObject star = GetAChild(starback);
+
+
+
+                    //初始化星星内容
+                    star.GetComponent<WinStar>().StarId = i+1;
+                    star.GetComponent<WinStar>().Delay = GetAChild(_starSample).GetComponent<WinStar>().Delay + i * AnimInterval;
+                    star.GetComponent<WinStar>().StarNumber = StarNumber;
+               
+
+
+
+//                    StarObjectsList.Add(star);
+
+
+                }
+               
             }
+
         }
     }
 
@@ -30,4 +76,15 @@ public class StarControl : MonoBehaviour
     void Update()
     {
     }
+
+    void IinitializeStar()
+    {
+        
+    }
+
+    //返回物体子对象
+        GameObject GetAChild(GameObject obj)
+        {
+            return (from Transform child in obj.transform select child.gameObject).FirstOrDefault();
+        }
 }
